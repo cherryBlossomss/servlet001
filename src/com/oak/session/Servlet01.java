35589c87a472503b4175495f36ceacec60b58ac1
cherryBlossomss <2712827632@qq.com>
2018-10-17 15:12:47 +0800

    Initial commit

---
+++ src/com/oak/session/Servlet01.java
@@ -0,0 +1,22 @@
+package com.oak.session;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet01 extends HttpServlet {
+   @Override
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	super.doGet(request, response);
+   }
+   @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(request, response);
+	}
+
+}
